18776a6627fd596848470f99ff0d98c20975988b
myth-faceless <mythmanish743@gmail.com>
2023-12-29 18:40:57 +0545

    Register page bug fixed

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -32,22 +32,22 @@ const Register = () => {
                     
                     <div class="mb-3  col-lg-6 col-md-6 col-12">
                         <label for="exampleInputPassword1" className="form-label">Email</label>
-                        <input type="email" className="form-control" id="email" name='email' value={getData.email} />
+                        <input type="email" className="form-control" id="email" name='email' value={getData.email} onChange={setdata} />
                     </div>
 
                     <div class="mb-3  col-lg-6 col-md-6 col-12">
                         <label for="exampleInputPassword1" className="form-label">Contact No:</label>
-                        <input type="text" className="form-control" id="contact" name='contact' value={getData.contact} />
+                        <input type="text" className="form-control" id="contact" name='contact' value={getData.contact} onChange={setdata} />
                     </div>
 
                     <div class="mb-3  col-lg-6 col-md-6 col-12">
                         <label for="exampleInputPassword1" clclassNameass="form-label">Address</label>
-                        <input type="text" className="form-control" id="address" name='address' value={getData.address} />
+                        <input type="text" className="form-control" id="address" name='address' value={getData.address} onChange={setdata} />
                     </div>
 
                     <div class="mb-3  col-lg-12 col-md-12 col-12">
                         <label for="exampleInputPassword1" className="form-label">Description</label>
-                        <textarea name="description"  className='form-control' id="description" cols="30" rows="5" value={getData.description} ></textarea>
+                        <textarea name="description"  className='form-control' id="description" cols="30" rows="5" value={getData.description} onChange={setdata} ></textarea>
                     </div>
                 
                     <button type="submit" className="btn btn-primary">Submit</button>
