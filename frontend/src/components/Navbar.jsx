a979935035f86f751c8391b35ffb7b064399d51b
myth-faceless <mythmanish743@gmail.com>
2024-01-03 11:06:58 +0545

    nav bar button style

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,20 +7,20 @@ const Navbar = () => {
     <header>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">Navbar</a>
+          <a className="navbar-brand" href="#">CRUD Operations</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item list" >
-                <NavLink to="/" style={{textDecoration:'none'}} className={'mx-2'}>
+                <NavLink to="/"  className="btn btn-outline-success">
                   <span>Home</span>
                 </NavLink>
 
-                <NavLink to="register" style={{textDecoration:'none'}}>
+                {/* <NavLink to="register" style={{textDecoration:'none'}}>
                   <span>Register</span>
-                </NavLink>
+                </NavLink> */}
 
               </li>
             
